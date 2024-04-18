bb92258b4815704f1cc4772d43940ff3d4531d58
Apolo <kesh.andrade@gmail.com>
2024-04-18 19:42:12 +0200

    yeah

---
+++ JavaGUIApp/src/main/java/com/entornografico/javaguiapp/LoginAndSignUp.java
@@ -0,0 +1,17 @@
+package com.entornografico.javaguiapp;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+/**
+ *
+ * @author Asuki
+ */
+public class LoginAndSignUp {
+
+    public static void main(String[] args) {
+        Login loginFrame = new Login();
+        loginFrame.setLocationRelativeTo(null); //center
+        loginFrame.setVisible(true);
+    }
+}
